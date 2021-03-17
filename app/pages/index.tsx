71f8389ab37922c88a6ac60b1c0adab68ffc7573
시혁 박 <exon.indiv@gmail.com>
2021-03-17 22:14:56 +0900

    Design: blog

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,7 +1,50 @@
 const IndexPage = () => (
-    <div className="center">
-        <b><h1>Intron</h1></b>
-        <b><p>엑슨의 블로그</p></b>
+    <div>
+        <div className="menu">
+            <img id="logo" src="./images/logo2.png" height="60em" />
+            <a>소개</a>
+            <a>프로젝트</a>
+            <a>연락</a>
+            <button id="changetheme" className="theme" ></button>
+        </div>
+
+        <div className="side">
+            <p className="group">소개</p>
+            <p><a href="">머릿말</a></p>
+            <p className="group">프로젝트</p>
+            <p><a href="">프로젝트</a></p>
+            <p><a href="">프로젝트 일지</a></p>
+            <p className="group">연락</p>
+            <p><a href="">연락처</a></p>
+        </div>
+
+        <div className="main">
+            <p className="title">머릿말</p>
+            <p className="content">내용</p>
+            <div className="card">
+                <p className="card-title">제목</p>
+                <p className="card-content">내용</p>
+            </div>
+            <div className="card">
+                <p className="card-title">제목</p>
+                <p className="card-content">내용</p>
+            </div>
+            <div className="card">
+                <p className="card-title">제목</p>
+                <p className="card-content">내용</p>
+            </div>
+            <div className="card">
+                <p className="card-title">제목</p>
+                <p className="card-content">내용</p>
+            </div>
+            <div className="card">
+                <p className="card-title">제목</p>
+                <p className="card-content">내용</p>
+            </div>
+        </div>
+        
+        <footer>
+        </footer>
     </div>
 )
 
